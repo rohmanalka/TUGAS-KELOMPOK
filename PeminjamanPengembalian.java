3f5c98f6294caaa24f2b5e192f650cdbcfa027fd
rohmanalka <rohmanalka06@gmail.com>
2023-11-24 10:10:17 +0700

    penambahan perulangan1

--- PeminjamanPengembalian.java
+++ PeminjamanPengembalian.java
@@ -46,20 +46,35 @@ public class PeminjamanPengembalian {
             System.out.println("\n=======INFORMASI LENGKAP=======");
             System.out.println("Nama Peminjam: " + nama + "\nNIM Peminjam: " + NIM + "\nBuku Yang Dipinjam: " + namaBuku + 
                                 "\nJumlah Buku Yang Dipinjam: " + jumlahBuku + "\nTotal Denda: " + denda);
-<<<<<<< HEAD
-            System.out.print("Masukkan nominal pembayaran: ");
-            int bayar = input.nextInt();
-            if (bayar > denda) {
-                double kembalian = bayar - denda;
-                System.out.println("Kembalian anda adalah: " + kembalian);
+            System.out.print("\nApakah anda ingin membayar denda? (ya/tidak): ");
+            String jawaban = input.next();
+
+            if (jawaban.equalsIgnoreCase("ya")) {
+                System.out.print("Masukkan nominal pembayaran anda: ");
+                int bayar = input.nextInt();
+            
+                while (bayar < denda){
+                    System.out.println("Nominal pembayaran anda tidak mencukupi, silahkan masukkan nominal yang mencukupi");
+                    System.out.print("Masukkan nominal pembayaran anda: ");
+                    bayar = input.nextInt();
+                }
+                if (bayar > denda) {
+                    double kembalian = bayar - denda;
+                    System.out.println("Kembalian anda adalah: " + kembalian);
+                    System.out.println("Buku berhasil dikembalikan");
+                } else {
+                    System.out.print("Uang anda pas, ");
+                    System.out.print("Buku berhasil dikembalikan");
+                }
+            } else if (jawaban.equalsIgnoreCase("tidak")) {
+                System.out.println("Anda memilih untuk tidak membayar denda \n" +nama+ "berhutang denda sebesar: " +denda);
                 System.out.println("Buku berhasil dikembalikan");
             } else {
-                System.out.print("Uang anda pas, ");
-                System.out.print("Buku berhasil dikembalikan");                }
-=======
->>>>>>> 0b8dd4aab95e3f209152d232410f060120a4d0ca
+                System.out.println("Input tidak valid, Buku berhasil dikembalikan tanpa pembayaran");
+            }
         } else {
-            System.out.println("Buku berhasil dikembalikan");
+            System.out.print("\n=======INFORMASI LENGKAP======= \nNama Peminjam: " + nama + "\nNIM Peminjam: " + NIM +
+            "\nBuku Yang Dipinjam: " + namaBuku + "\nJumlah Buku Yang Dipinjam: " + jumlahBuku + "\nBuku Berhasil Dikembalikan");
         }
     }
 }
